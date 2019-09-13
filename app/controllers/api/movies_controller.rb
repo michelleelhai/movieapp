383281515ef869dc3dcd2e6690638724ba79d797
Mishy <michelleelhai@gmail.com>
2019-09-12 20:58:20 -0700

    adds authentification

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,4 +1,5 @@
 class Api::MoviesController < ApplicationController
+  before_action :authenticate_admin, except: [:index, :show]
 def show
     @movie = Movie.find_by(id: params[:id])
     render 'show.json.jb'
