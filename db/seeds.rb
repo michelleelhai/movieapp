e83567991aac3e452cf950defbbf48db0e4f9031
Mishy <michelleelhai@gmail.com>
2019-09-15 17:03:21 -0700

    adds through associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,20 @@
 # actor.save
 # actor = Actor.new({first_name: "Naomi", last_name: "Scott", known_for: "Jasmine"})
 # actor.save
-movie = Movie.new({title: "Lion King", year: 2019, plot: "A young lion's journey to becoming king of his pride"})
-movie.save
-movie = Movie.new({title: "Moana", year: 2016, plot: "A girl who likes water kills a volcano and then it turns into a mountain"})
-movie.save
-movie = Movie.new({title:"Aladdin", year: 2019, plot: "a theif and a princess fall in love and ride around on a magic carpet."})
-movie.save
+# movie = Movie.new({title: "Lion King", year: 2019, plot: "A young lion's journey to becoming king of his pride"})
+# movie.save
+# movie = Movie.new({title: "Moana", year: 2016, plot: "A girl who likes water kills a volcano and then it turns into a mountain"})
+# movie.save
+# movie = Movie.new({title:"Aladdin", year: 2019, plot: "a theif and a princess fall in love and ride around on a magic carpet."})
+# movie.save
+# Genre.create({name: "romance"})
+# Genre.create({name: "sci-fi"})
+# Genre.create({name: "Kids"})
+# Genre.create({name: "drama"})
+MovieGenre.create({movie_id: 1, genre_id: 2})
+MovieGenre.create({movie_id: 2, genre_id: 3})
+MovieGenre.create({movie_id: 3, genre_id: 4})
+MovieGenre.create({movie_id: 5, genre_id: 1})
+MovieGenre.create({movie_id: 6, genre_id: 2})
+MovieGenre.create({movie_id: 7, genre_id: 3})
+MovieGenre.create({movie_id: 8, genre_id: 1})
