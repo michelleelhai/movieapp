f3b86d609643a927bb6c08aef1eceb2fecbb5e6d
Mishy <michelleelhai@gmail.com>
2019-09-18 22:58:11 -0700

    adds html

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -2,7 +2,7 @@ class Api::MoviesController < ApplicationController
   before_action :authenticate_admin, except: [:index, :show]
 def show
     @movie = Movie.find_by(id: params[:id])
-    render 'show.json.jb'
+    render 'show.html.erb'
   end
   def index
     @movies = Movie.all
